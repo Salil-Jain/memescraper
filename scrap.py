330d89407843752c23e46487bd4b6e1c2291f6ab
Salil-sopho <j.salil36@gmail.com>
2017-06-17 12:41:58 +0530

    Option for number of pages added

--- scrap.py
+++ scrap.py
@@ -2,24 +2,29 @@ import urllib
 import re
 import subprocess
 import time
+import sys
 
 urls="https://www.reddit.com/r/dankmemes/"
 
 i=0
-while i<10:
-	these_regex="data-url=\"(.+?)\""
-	pattern=re.compile(these_regex)
-	htmlfile=urllib.urlopen(urls)
-	htmltext=htmlfile.read()
-	titles=re.findall(pattern,htmltext)
-	for s in titles:
-		com = "wget " + s
-		subprocess.call(com,shell=True)
-	regex1 = "next-button.+?\"(.+?)\""
-	pattern1 = re.compile(regex1)
-	link1=re.findall(pattern1,htmltext)
-	if(len(link1)==0):
-		print "Something went wrong for i = %d. trying again..."%i
-	else:
-		urls = link1[0]
-		i+=1
+num = input("How many pages would you like to download(25 memes per page)?")
+if num<1:
+	sys.exit("Number of pages should be > 0")
+else :
+	while i<num:
+		these_regex="data-url=\"(.+?)\""
+		pattern=re.compile(these_regex)
+		htmlfile=urllib.urlopen(urls)
+		htmltext=htmlfile.read()
+		titles=re.findall(pattern,htmltext)
+		for s in titles:
+			com = "wget " + s
+			subprocess.call(com,shell=True)
+		regex1 = "next-button.+?\"(.+?)\""
+		pattern1 = re.compile(regex1)
+		link1=re.findall(pattern1,htmltext)
+		if(len(link1)==0):
+			print "Something went wrong for i = %d. trying again..."%i
+		else:
+			urls = link1[0]
+			i+=1
