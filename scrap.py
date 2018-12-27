2e5968f5b746afe8eda1076c7ed62a4fd870efa7
khizar-anjum <khizaranjum28@gmail.com>
2018-12-27 12:19:42 +0500

    added log functionality

--- scrap.py
+++ scrap.py
@@ -3,29 +3,44 @@ import re
 import subprocess
 import time
 import sys
+import os.path
+import pandas as pd
 
 urls="https://old.reddit.com/r/dankmemes/"
-
+if(os.path.isfile('../url_log.csv')):
+	log = pd.read_csv('../url_log.csv');
+	urls = log.iloc[-1].values[1];
+	list_log = [];
+else:
+	list_log = [urls];
 i=0
 num = input("How many pages would you like to download(25 memes per page)?")
 if num<1:
 	sys.exit("Number of pages should be > 0")
 else :
-	while i<num:
-		these_regex="data-url=\"(.+?)\""
-		pattern=re.compile(these_regex)
-		htmlfile=urllib.urlopen(urls)
-		htmltext=htmlfile.read()
-		titles=re.findall(pattern,htmltext)
-		for s in titles:
-			com = "wget --no-check-certificate " + s
-			subprocess.call(com,shell=True)
-		regex1 = "next-button.+?\"(.+?)\""
-		pattern1 = re.compile(regex1)
-		link1=re.findall(pattern1,htmltext)
-		if(len(link1)==0):
-			print "Something went wrong for i = %d. trying again..."%i
-			time.sleep(3)
-		else:
-			urls = link1[0]
-			i+=1
+	try:
+		while i<num:
+			these_regex="data-url=\"(.+?)\""
+			pattern=re.compile(these_regex)
+			htmlfile=urllib.urlopen(urls)
+			htmltext=htmlfile.read()
+			titles=re.findall(pattern,htmltext)
+			for s in titles:
+				com = "wget --no-check-certificate " + s
+				subprocess.call(com,shell=True)
+			regex1 = "next-button.+?\"(.+?)\""
+			pattern1 = re.compile(regex1)
+			link1=re.findall(pattern1,htmltext)
+			if(len(link1)==0):
+				print "Something went wrong for i = %d. trying again..."%i
+				time.sleep(3)
+			else:
+				urls = link1[0]
+				list_log.append(urls);
+				i+=1
+		log = pd.Series(list_log);
+		log.to_csv('../url_log.csv',mode = 'a');
+	except:
+		print "exception occured: saving log";
+		log = pd.Series(list_log);
+		log.to_csv('../url_log.csv',mode = 'a');
