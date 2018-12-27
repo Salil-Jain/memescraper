5b5be0e9dd164334f70aaa4bb558fac125496179
khizar-anjum <khizaranjum28@gmail.com>
2018-12-27 15:01:14 +0500

    added name functionality

--- scrap.py
+++ scrap.py
@@ -5,14 +5,29 @@ import time
 import sys
 import os.path
 import pandas as pd
+from bs4 import BeautifulSoup
 
-urls="https://old.reddit.com/r/dankmemes/"
+#now, we can scrape any memes!
+subreddit = raw_input("Please enter the name of subreddit to scrape: ");
+if not isinstance(subreddit, str):
+	sys.exit('Please enter a valid string')
+
+#the url of the website to be scraped!
+urls="https://old.reddit.com/r/" + subreddit + "/"
+
+#a log functionality to return scraping as soon as restarted
+#delete the url_log.csv file to restart the process!
 if(os.path.isfile('../url_log.csv')):
 	log = pd.read_csv('../url_log.csv');
-	urls = log.iloc[-1].values[1];
+	try:
+		urls = log.iloc[-1].values[1];
+	except:
+		urls = log.columns[1];
 	list_log = [];
 else:
 	list_log = [urls];
+
+# input the number of pages to scrape memes from!
 i=0
 num = input("How many pages would you like to download(25 memes per page)?")
 if num<1:
@@ -24,10 +39,18 @@ else :
 			pattern=re.compile(these_regex)
 			htmlfile=urllib.urlopen(urls)
 			htmltext=htmlfile.read()
+			content = htmltext;
+			#using soup to find names!
+			soup = BeautifulSoup(content,'lxml')
+			names = soup.find_all(["p","a"],{'class':'title','data-event-action':'title'});
 			titles=re.findall(pattern,htmltext)
-			for s in titles:
-				com = "wget --no-check-certificate " + s
-				subprocess.call(com,shell=True)
+			for i,s in enumerate(titles):
+				try:
+					com = "wget --no-check-certificate " + s + " -O \"" + names[i].get_text() + "\".jpg";
+					subprocess.call(com,shell=True)
+				except:
+					com = "wget --no-check-certificate " + s;
+					subprocess.call(com,shell=True)
 			regex1 = "next-button.+?\"(.+?)\""
 			pattern1 = re.compile(regex1)
 			link1=re.findall(pattern1,htmltext)
@@ -40,7 +63,8 @@ else :
 				i+=1
 		log = pd.Series(list_log);
 		log.to_csv('../url_log.csv',mode = 'a');
-	except:
-		print "exception occured: saving log";
+	except Exception as e:
+		print e;
+		print "saving log";
 		log = pd.Series(list_log);
 		log.to_csv('../url_log.csv',mode = 'a');
